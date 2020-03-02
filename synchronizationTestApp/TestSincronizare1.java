ed22bdd2210708775fc85de5298b4b46c0a5ce00
Teodora Pasalan <49790698+TeodoraTH3O@users.noreply.github.com>
2020-03-02 15:29:52 +0200

    Synchronization Test Application

---
+++ synchronizationTestApp/TestSincronizare1.java
@@ -0,0 +1,13 @@
+package main.java.exemplul6;
+
+public class TestSincronizare1 {
+
+    public static void main(String[] args) {
+        Buffer b = new Buffer();
+        Producator p1 = new Producator(b);
+        Consumator c1 = new Consumator(b);
+
+        p1.start();
+        c1.start();
+    }
+}
